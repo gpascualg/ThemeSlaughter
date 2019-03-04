946d04211f89357ffb7a2300db81d323cb4aa700
gpascualg <gpascualg93@gmail.com>
2019-03-04 22:58:27 +0100

    Dynamically show round

--- app/src/index.py
+++ app/src/index.py
@@ -1,11 +1,11 @@
 from flask import render_template, request, jsonify, abort
 from flask.views import MethodView
 
-from .context import build_context
+from .context import build_context, get_config
 from .database import Database
 from .authentication import force_login
 
 
 class Index(MethodView):
     def get(self):
-        return render_template('index.html', **build_context())
+        return render_template('index.html', **build_context({"round": get_config("voting_round")}))
